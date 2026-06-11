ecbd6947cf322b0eac3c62132b620297041a3256
marceluj <marcel.belica@student.uj.edu.pl>
2026-06-11 16:56:11 +0200

    Add Spring Boot backend

---
+++ src/test/java/com/codibly/energy_backend/EnergyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codibly.energy_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnergyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
